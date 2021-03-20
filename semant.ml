18332299fd45dac8eb3055a15c84aab340282c60
SheronW <sheronw1174@gmail.com>
2021-03-20 16:02:17 -0400

    fix type for built in semant

--- semant.ml
+++ semant.ml
@@ -3,7 +3,8 @@
 open Ast
 open Sast
 
-(* A map for global variables (VarDef, FxnDef, Alias, StructDef) - we don't have scope defined so a string type pair is enough? *)
+(* import map for global variables (VarDef, FxnDef, Alias, StructDef) *)
+(* we don't have scope defined so a string * tid is enough? *)
 module StringMap = Map.Make(String)
 
 (* Semantic checking of the AST. Returns an SAST if successful,
@@ -11,12 +12,12 @@ module StringMap = Map.Make(String)
 
    Check each statement *)
 
-let check stmt =
+let check stmts =
 
   (* add built-in function such as basic printing *)
 
   let built_in_decls = 
     let add_bind map (name, ty) = StringMap.add name ty map
-    in List.fold_left add_bind StringMap.empty [ ("print", "int");
-			                         ("printb", "bool");
-			                         ("printf", "float")]
+    in List.fold_left add_bind StringMap.empty [ ("print", TypeID("int"));
+			                         ("printb", TypeID("bool"));
+			                         ("printf", TypeID("float"))]
