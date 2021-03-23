5e615fcb82f63aae05b03f0d8a60f618e7873b05
SheronW <sheronw1174@gmail.com>
2021-03-23 15:25:48 -0400

    add template for codegen

--- codegen.ml
+++ codegen.ml
@@ -1,2 +1,21 @@
 (* Code generation: translate takes a semantically checked AST and
 produces LLVM IR *)
+module L = Llvm
+module A = Ast
+open Sast
+
+module StringMap = Map.Make(String)
+
+(* translate : Sast.program -> Llvm.module *)
+let translate stmts =
+  let context = L.global_context () in
+
+  let the_module = L.create_module context "SOS" in
+  (* *)
+
+  let build_stmt stmtdecl =
+  (* *)
+  
+  in
+  List.iter build_stmt stmts;
+  the_module
