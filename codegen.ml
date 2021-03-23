ede32bccefae0721337558d237d054db193fe52d
SheronW <sheronw1174@gmail.com>
2021-03-23 16:30:11 -0400

    copy and paste llvm type and external function def

--- codegen.ml
+++ codegen.ml
@@ -11,7 +11,27 @@ let translate stmts =
   let context = L.global_context () in
 
   let the_module = L.create_module context "SOS" in
-  (* *)
+
+  (* Get types from the context *)
+  let i32_t      = L.i32_type    context
+  and i8_t       = L.i8_type     context
+  and i1_t       = L.i1_type     context
+  and float_t    = L.double_type context
+  and void_t     = L.void_type   context in
+
+  (* Return the LLVM type for a MicroC type *)
+  let ltype_of_typ = function
+      A.Int   -> i32_t
+    | A.Bool  -> i1_t
+    | A.Float -> float_t
+    | A.Void  -> void_t
+  in
+
+  (* declare external functions *)
+  let printf_t : L.lltype = 
+      L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
+  let printf_func : L.llvalue = 
+      L.declare_function "printf" printf_t the_module in
 
   let build_stmt stmtdecl =
   (* *)
